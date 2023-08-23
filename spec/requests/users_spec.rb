29ce250dfd9a0685acbb50f5cca109abd6b7813f
Hanse Kim <procrustes5@gmail.com>
2023-08-23 15:48:22 +0900

    rubocop -a

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
- 
-RSpec.describe "Users", type: :request do
-  describe "GET /signup" do
-    it "returns http success" do
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /signup' do
+    it 'returns http success' do
       get signup_path
       expect(response).to have_http_status(:ok)
     end
