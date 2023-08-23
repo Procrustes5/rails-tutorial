4cc01563bc8a87534efcb20f34a4adb14171d751
Hanse Kim <procrustes5@gmail.com>
2023-08-23 14:59:56 +0900

    6.2 Temp Save

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
-
+ 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Example User', email: 'user@example.com') }
+ 
+  it 'userが有効であること' do
+    expect(user).to be_valid
+  end
+
+  it 'nameが必須であること' do
+    user.name = ''
+    expect(user).to_not be_valid
+  end
 end
