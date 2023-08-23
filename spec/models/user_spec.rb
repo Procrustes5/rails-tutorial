0d3690259db98a99fbbe3c4c4f97d305f34a3e07
Hanse Kim <procrustes5@gmail.com>
2023-08-23 16:31:14 +0900

    6.2 User Validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
- 
+
 RSpec.describe User, type: :model do
   let(:user) { User.new(name: 'Example User', email: 'user@example.com') }
- 
+
   it 'userが有効であること' do
     expect(user).to be_valid
   end
@@ -10,5 +10,50 @@ RSpec.describe User, type: :model do
   it 'nameが必須であること' do
     user.name = ''
     expect(user).to_not be_valid
+  end
+  it 'emailが必須であること' do
+    user.email = ''
+    expect(user).to_not be_valid
+  end
+  it 'nameは50文字以内であること' do
+    user.name = 'a' * 51
+    expect(user).to_not be_valid
+  end
+
+  it 'emailは255文字以内であること' do
+    user.email = "#{'a' * 244}@example.com"
+    expect(user).to_not be_valid
+  end
+
+  it 'emailが有効な形式であること' do
+    valid_addresses = %w[user@exmple.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      expect(user).to be_valid
+    end
+  end
+  it '無効な形式のemailは失敗すること' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      user.email = invalid_address
+      expect(user).to_not be_valid
+    end
+  end
+  it 'emailは重複して登録できないこと' do
+    duplicate_user = user.dup
+    user.save
+    expect(duplicate_user).to_not be_valid
+  end
+  it 'emailは重複して登録できないこと' do
+    duplicate_user = user.dup
+    duplicate_user.email = user.email.upcase
+    user.save
+    expect(duplicate_user).to_not be_valid
+  end
+  it 'emailは小文字でDB登録されていること' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    user.email = mixed_case_email
+    user.save
+    expect(user.reload.email).to eq mixed_case_email.downcase
   end
 end
