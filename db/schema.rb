39bbfcacbdb51ee0b3560bc5afcc2265d62e1aa2
Hanse Kim <procrustes5@gmail.com>
2023-09-12 17:02:01 +0900

    null false

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2023_09_12_050510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
-    t.boolean "admin", default: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
