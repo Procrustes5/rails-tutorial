d2d1b56d9df71f73fb958f10424fcc026d240a49
Hanse Kim <procrustes5@gmail.com>
2023-09-19 10:31:47 +0900

    Create Microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  user_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Micropost < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,39 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  user_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:micropost) { user.microposts.build(content: "Lorem ipsum") }
+
+  it '有効であること' do
+    expect(micropost).to be_valid
+  end
+
+  it 'user_idが存在する' do
+    micropost.user_id = nil
+    expect(micropost).to_not be_valid
+  end
+
+  describe "content" do
+    it '内容が空なら無効であること' do
+      micropost.content = '    '
+      expect(micropost).to_not be_valid
+    end
+
+    it '内容は140字までにする' do
+      micropost.content = 'a' * 141
+      expect(micropost).to_not be_valid
+    end
+  end
+
+
 end
