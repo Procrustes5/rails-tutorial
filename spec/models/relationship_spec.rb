40eebae1eadb9137b897bbad60b3f7760db7f9da
Hanse Kim <procrustes5@gmail.com>
2023-09-20 10:16:21 +0900

    Validation test

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,33 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :bigint           not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user)}
+  let(:other_user) { FactoryBot.create(:user, :another)}
+  before do
+    @relationship = Relationship.new(follower_id: user.id, followed_id: other_user.id)
+  end
+
+  it "Relationshipモデルが有効であること" do
+    expect(@relationship).to be_valid
+  end
+
+  it "follower_idが必須であること" do
+    @relationship.follower_id = nil
+    expect(@relationship).to_not be_valid
+  end
+
+  it "followed_idが必須であること" do
+    @relationship.followed_id = nil
+    expect(@relationship).to_not be_valid
+  end
 end
