05dabfcf97b1ecdfed769239b0c2883b93b360e4
Hanse Kim <procrustes5@gmail.com>
2023-09-20 11:13:45 +0900

    Factory, Static Page Test

---
+++ spec/factories/relationships.rb
@@ -0,0 +1,27 @@
+FactoryBot.define do
+  factory :follower, class: Relationship do
+    follower_id { 1 }
+    followed_id { 1 }
+  end
+ 
+  factory :following, class: Relationship do
+    follower_id { 1 }
+    followed_id { 1 }
+  end
+end
+ 
+def create_relationships
+  users = []
+  10.times do
+    users << FactoryBot.create(:continuous_users)
+  end
+
+  FactoryBot.create(:user) do |user|
+    users[0..-1].each do |other|
+      FactoryBot.create(:follower, follower_id: other.id, followed_id: user.id)
+      FactoryBot.create(:following, follower_id: user.id, followed_id: other.id)
+    end
+    user
+  end
+
+end
