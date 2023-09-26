6466df49e83ae6dbb38331efd42fe6d1ab9f41c3
Hanse Kim <procrustes5@gmail.com>
2023-09-26 14:16:07 +0900

    Merge branch 'Rspec-reinforce' into Chapter14

--- spec/system/following_spec.rb
+++ spec/system/following_spec.rb
@@ -6,14 +6,33 @@ RSpec.describe 'Following', type: :system do
     @user = FactoryBot.send(:create_relationships)
     log_in @user
   end
-
   describe 'following and followers' do
-    it 'followingの数とフォローしているユーザへのリンクが表示されていること' do
-      visit following_user_path(@user)
-      expect(@user.following).to_not be_empty
-      expect(page).to have_content '10 following'
-      @user.following.each do |user|
-        expect(page).to have_link user.name, href: user_path(user)
+    context 'フォロワーが多数いる場合' do
+      it 'followingの数とフォローしているユーザへのリンクが表示されていること' do
+        visit following_user_path(@user)
+        expect(@user.following).to_not be_empty
+        expect(page).to have_content '10 following'
+        @user.following.each do |user|
+          expect(page).to have_link user.name, href: user_path(user)
+        end
+      end
+    end
+
+    context 'フォロワーが複数ではない場合' do
+      let(:user) { FactoryBot.create(:user, :unrelated) }
+      let(:other) { FactoryBot.create(:user, :another) }
+      it 'フォロワーがゼロの時は単数形で表示' do
+        visit following_user_path(user)
+        expect(user.following).to be_empty
+        expect(page).to_not have_content 'followers'
+      end
+
+      it 'フォロワーが1名の時は単数形で表示' do
+        FactoryBot.create(:follower, follower_id: other.id, followed_id: user.id)
+        FactoryBot.create(:following, follower_id: user.id, followed_id: other.id)
+        visit following_user_path(user)
+        expect(user.following.count).to eq(1)
+        expect(page).to_not have_content 'followers'
       end
     end
   end
