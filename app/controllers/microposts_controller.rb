c9d98f1c6b1db275b49021f22c018c78eefdacfe
Hanse Kim <procrustes5@gmail.com>
2023-09-26 14:18:51 +0900

    modified method name

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user
-  before_action :correct_user, only: :destroy
+  before_action :micropost_of_correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -26,7 +26,7 @@ class MicropostsController < ApplicationController
     params.require(:micropost).permit(:content, :picture)
   end
 
-  def correct_user
+  def micropost_of_correct_user
     @micropost = current_user.microposts.find_by(id: params[:id])
     redirect_to root_url if @micropost.nil?
   end
